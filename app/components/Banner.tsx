8ced7a3b449dad04e46c9f177132e7bfd8ee84c3
RiazullJannat <ramimk097@gmail.com>
2026-07-14 16:29:45 +0600

    added comment

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -38,6 +38,7 @@ const Banner = () => {
                 <div
                     className="w-[70vw] md:w-[50vw] lg:w-[50vw] mx-auto h-[18vh] md:h-[19vh] lg:h-[35vh] xl:h-[40vh] 2xl:h-[47vh] relative flex justify-between"
                 >
+                    //dfdfdf
 
                     <Image
                         src={bannerImage} alt="rjannat"
